d279671ba1c64a2560905259a89560f1d29abcc5
lfs <luciano2023silva@gmail.com>
2023-01-17 20:00:39 -0300

    refactor: isolando rotas backend

---
+++ backend/README.md
@@ -0,0 +1,14 @@
+Regras de negócio:
+A aplicação é feita para mais de um usuário por ora.
+Tera uma entidade Habito
+uma tabela para armzenar cada habito/task emk cada dia
+entao cada dia pode ter varios habitos e esses habitos podem estar completos ou não
+Então termos a tabela/model:
+
+
+Habit
+Day
+DayHabit
+
+Criação do diagrama ERD
+Criação de seeds (dados para popular o meu banco)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,11 @@
 import Fastify from 'fastify';
-
-import { PrismaClient } from '@prisma/client';
-
 import cors from '@fastify/cors'
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
 
 app.listen({port: 3333}).then(() => console.log('server running in port: 3333'))
