1d7aa166752a98da40859d70104bdf298f89fea1
lfs <luciano2023silva@gmail.com>
2023-01-19 06:02:12 -0300

    feat: Header HabitDay SummaryTable components

--- front-end-web/src/App.tsx
+++ front-end-web/src/App.tsx
@@ -1,19 +1,16 @@
-import { Habit } from './components/Habit';
 
 import './styles/global.scss';
+import { Header } from './components/Header';
+import { SummayTable } from './components/SummaryTable';
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={5} />
-      <Habit completed={2} />
-      <Habit completed={4} />
-      <Habit completed={10} />
-      <Habit completed={2} />
-      <Habit completed={9} />
-      <Habit completed={8} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummayTable />
+      </div>
     </div>
   );
 }
 
-export default App;
---
+++ front-end-web/src/components/HabitDay.tsx
@@ -0,0 +1,6 @@
+export function HabitDay() {
+  return (
+    <div className='h-10 w-10 bg-gray-900 border-2 border-zinc-800 rounded-lg'></div>
+
+  )
+}
