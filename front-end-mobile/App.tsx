808c95217a177e97afe4d4b69f634fd0a95e7fa0
lfs <luciano2023silva@gmail.com>
2023-01-19 18:29:05 -0300

    feat: create components

--- front-end-mobile/App.tsx
+++ front-end-mobile/App.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import './src/lib/dayjs';
+import { StatusBar } from 'react-native';
 
 import { 
   useFonts, 
@@ -8,36 +9,27 @@ import {
   Inter_800ExtraBold
  } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   // as fontes precisa carregar antes do app ser renderizado
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
-    Inter_700Bold, 
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
     Inter_800ExtraBold
   });
 
-  if (fontsLoaded) {
-    <Loading />
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
--- front-end-mobile/README.md
+++ front-end-mobile/README.md
@@ -1,1 +1,33 @@
 - [NativeWind](https://www.nativewind.dev/)
+
+
+
+
+```js
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090a',
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      }
+    },
+  },
+  plugins: [],
+}
+
+```
+
+só paramos de rodar o app para instalar libs que tocam no código nativo o dayjs como é puro js não tem a necessidade de para a aplicação
