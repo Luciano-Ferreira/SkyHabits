feb46d259a614dda271cdb0e39b500657cb21013
lfs <luciano2023silva@gmail.com>
2023-01-21 20:22:10 -0300

    feat: Habit Screen and ProgressBar

--- front-end-mobile/src/screens/Home.tsx
+++ front-end-mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, ScrollView } from 'react-native';
 import { DAY_SIZE, HabitDay } from '../components/HabitDay';
 import { Header } from '../components/Header';
@@ -10,6 +11,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -32,7 +35,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay 
+              key={date.toISOString()} 
+              onPress={() => navigate('habit', {date: date.toISOString()})}  
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, i) => (
