5ff4a3b193136085ec96cb6bd224b5114ae73f03
lfs <luciano2023silva@gmail.com>
2023-01-22 22:00:41 -0300

    styles: focus transitions styles

--- front-end-web/src/components/NewHabitForm.tsx
+++ front-end-web/src/components/NewHabitForm.tsx
@@ -59,7 +59,7 @@ export function NewHabitForm() {
         type="text"
         id="title"
         placeholder="Ex: Exercicios, dormir bem, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         onChange={event => setTitle(event.target.value)}
         value={title}
@@ -73,13 +73,13 @@ export function NewHabitForm() {
         {availableWeekDay.map((Wd, i) => {
           return (
             <CheckBox.Root 
-              className="flex items-center gap-3 group" 
+              className="flex items-center gap-3 group focus:outline-none" 
               checked={weekDays.includes(i)}
               key={Wd}
               onCheckedChange={() => handleToggleWeekDay(i)}
             >
               <div 
-                className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500"
+                className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-green-600 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900"
               >
                 <CheckBox.Indicator>
                   <Check size={20} className="text-white" />
@@ -93,7 +93,7 @@ export function NewHabitForm() {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center font-semibold bg-green-600 justify-center gap-3 hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center font-semibold bg-green-600 justify-center gap-3 hover:bg-green-500 transition-colors"
       >
         <Check size={20} weight="bold" />
         Confirmar
